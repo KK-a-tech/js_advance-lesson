2349fb04f24a03b22783afa41fb555cf0b889be9
KK-a-tech <kento.k711@gmail.com>
2024-12-21 15:18:33 +0900

    feat:Lesson1_comp

--- src/lesson/defaultArgument.js
+++ src/lesson/defaultArgument.js
@@ -4,8 +4,8 @@
 
 
 
-
-
+// 割引後の値段を計算する関数discountRateCalcを実行し、4500円の5%引きと、6000円の20%引きの値をconsoleに出力してください。
+// ただし、割引率のデフォルト値に5%を設定すること。
 
 
 
@@ -13,6 +13,9 @@
 
 // 練習問題
 // 問題 1
-function discountRateCalc(price, rate) {
+function discountRateCalc(price, rate = 5) {
   return price * (1 - rate * 0.01);
 }
+
+console.log(discountRateCalc(4500));
+console.log(discountRateCalc(6000, 20));
