4fee4c45bb5deb59c6842d6c0daacc571c3cb15d
KK-a-tech <kento.k711@gmail.com>
2024-12-21 22:09:17 +0900

    feat:Lesson6-2_comp

--- src/lesson/esModules/defaultExport.js
+++ src/lesson/esModules/defaultExport.js
@@ -13,6 +13,10 @@
 
 // 練習問題
 // 問題 2
+// 下記条件を満たした状態で、関数defaultFnを実行し、返り値をconsoleに出力してください。
+// ・src/lesson/esModules/defaultExport.jsに定義されている関数defaultFnをdefault exportでエクスポート
 function defaultFn() {
   return 'default export';
 }
+
+export default defaultFn;
