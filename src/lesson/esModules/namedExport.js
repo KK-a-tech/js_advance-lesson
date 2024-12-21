a1579e38ef4a181b057c466f2691c26237a23b33
KK-a-tech <kento.k711@gmail.com>
2024-12-21 22:04:08 +0900

    feat:Lesson6-1_comp

--- src/lesson/esModules/namedExport.js
+++ src/lesson/esModules/namedExport.js
@@ -13,6 +13,9 @@
 
 // 練習問題
 // 問題 1
+// 下記条件を満たした状態で、関数nameFnとcalcFnを実行し、返り値をconsoleに出力してください。
+// ・src/lesson/esModules/namedExport.jsに定義されている関数nameFnとcalcFnをnamed exportでエクスポート
+
 const nameFn = () => {
   return 'Gizumo taro';
 };
@@ -20,3 +23,6 @@ const nameFn = () => {
 const calcFn = (num) => {
   return num * 10;
 };
+
+export { nameFn };
+export { calcFn };
--- src/lesson/esModules/namedImport.js
+++ src/lesson/esModules/namedImport.js
@@ -13,3 +13,10 @@
 
 // 練習問題
 // 問題 1
+// 下記条件を満たした状態で、関数nameFnとcalcFnを実行し、返り値をconsoleに出力してください。
+// ・src/lesson/esModules/namedImport.jsで関数nameFnとcalcFnをnamed importでインポート
+
+import { nameFn, calcFn } from './namedExport.js';
+
+console.log(nameFn());
+console.log(calcFn(5));
