e66b4dd8f834681bee41b22a95e47bc4daf275f2
KK-a-tech <kento.k711@gmail.com>
2024-12-21 22:41:03 +0900

    feat:Lesson7_comp

--- src/lesson/domOperation/index.js
+++ src/lesson/domOperation/index.js
@@ -1,14 +1,25 @@
 import 'animate.css';
 
 // ここにコードを書きながら確認しましょう！
+const title = document.getElementById('title');
+const button = document.getElementById('button');
+const cardMocha = document.querySelector('.card-type--mocha');
+const cardYellow = document.querySelector('.card-type--yellow');
 
+console.log(cardMocha);
+console.log(cardYellow);
 
-
-
-
-
-
-
+button.addEventListener('click', () => {
+  // title要素に.animate__hingeクラスを付与
+  title.classList.add('animate__hinge');
+  // 2秒後にtitle要素から.animate__hingeクラスを削除
+  setTimeout(() => {
+    title.classList.remove('animate__hinge');
+    cardMocha.classList.add('card-animation');
+    cardYellow.style.display = '';
+    cardYellow.classList.add('animate__fadeInUp');
+  }, 2000);
+});
 
 
 
