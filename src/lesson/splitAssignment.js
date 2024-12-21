d9097164e69772d965f63ea5364ca3d3b8bae38e
KK-a-tech <kento.k711@gmail.com>
2024-12-21 15:48:25 +0900

    feat:Lesson2-1_comp

--- src/lesson/splitAssignment.js
+++ src/lesson/splitAssignment.js
@@ -13,9 +13,15 @@
 
 // 練習問題
 // 問題 1
+// 関数arrayFnを実行し、返り値の配列から引数argが使用されている値を分割代入で取得し、consoleに出力してください。
+// 関数arrayFnに渡す引数に指定はありません。
 function arrayFn(arg) {
   return ['jQuery', 'Vue', arg];
 }
+
+const [val1, val2, val3] = arrayFn('hoge');
+
+console.log(val3);
 
 // 問題 2
 function objectFn() {
