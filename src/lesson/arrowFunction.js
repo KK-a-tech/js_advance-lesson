bc147ff4376d4b8f300a325122231b2d5207da0b
KK-a-tech <kento.k711@gmail.com>
2024-12-21 20:04:14 +0900

    feat:Lesson4_comp

--- src/lesson/arrowFunction.js
+++ src/lesson/arrowFunction.js
@@ -13,6 +13,14 @@
 
 // 練習問題
 // 問題 1
-const testFn = function(name) {
+// 関数testFnをアロー関数で書き直して、consoleに関数の実行結果を出力してください。
+// ※省略記法が使用できる場合は、省略記法を使用すること。
+// const testFn = function(name) {
+//   return `Gizumo ${name}`;
+// };
+
+const testFn = name => {
   return `Gizumo ${name}`;
 };
+
+console.log(testFn('hoge'));
