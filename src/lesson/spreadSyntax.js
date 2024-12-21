0162495cb9f1c5f4eed7e991cf5b68bca0d0a275
KK-a-tech <kento.k711@gmail.com>
2024-12-21 19:57:23 +0900

    feat:Lesson3-1_comp

--- src/lesson/spreadSyntax.js
+++ src/lesson/spreadSyntax.js
@@ -13,10 +13,13 @@
 
 // 練習問題
 // 問題 1
+// consoleにpackageManagerとframeworkがマージされた配列が出力されるように、関数arrayFnの返り値をスプレッド構文を使用して定義してください。
 const packageManager = ['npm', 'yarn', 'pnpm'];
 
 function arrayFn(array) {
   const framework = ['Vue', 'React', 'Svelte'];
+  const updatedCompanyInfo = [...array, ...framework];
+  return updatedCompanyInfo;
 }
 
 console.log(arrayFn(packageManager));
