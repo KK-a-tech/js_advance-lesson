b6a712b5fd5904e41166b65212721f920ccb2fe4
KK-a-tech <kento.k711@gmail.com>
2024-12-21 21:45:46 +0900

    feat:Lesson5_comp

--- src/lesson/arrayMethods.js
+++ src/lesson/arrayMethods.js
@@ -12,6 +12,8 @@
 
 // 練習問題
 // 問題 1
+// 下記条件を満たした配列をmapメソッドを使用して作成し、consoleに出力してください。
+// ・プロパティclassNameのvalueにc-というプレフィックスを付与
 const array = [
   {
     tag: 'p',
@@ -26,3 +28,9 @@ const array = [
     className: 'piyo',
   },
 ];
+
+const newArray = array.map(array => {
+  return {...array, className:'c-' + array.className};
+});
+
+console.log(newArray);
