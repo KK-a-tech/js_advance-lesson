f61441d4bac14415e69e8b4b5bfb119d984fbc06
KK-a-tech <kento.k711@gmail.com>
2024-12-22 09:44:59 +0900

    feat:Lesson8_comp

--- src/lesson/promise/index.js
+++ src/lesson/promise/index.js
@@ -13,3 +13,16 @@
 
 // 練習問題
 // 問題 1
+// 関数returnPromiseを実行し、非同期処理の結果である「エラーが発生しました」をconsoleに出力してください。
+// ただし、関数returnPromiseは編集しないこと
+
+const returnPromise = () => {
+  return new Promise((_resolve, reject) => {
+    setTimeout(() => {
+      reject('エラーが発生しました');
+    }, 3000);
+  });
+};
+
+returnPromise()
+  .catch(err => console.log(err));
